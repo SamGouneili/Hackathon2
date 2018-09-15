6718065f5d02cecb343b29f41f84c5b8a9eaefed
Sam Gouneili <samgouneili@gmail.com>
2018-09-15 14:20:44 -0700

    Initial commit

---
+++ app/src/main/java/com/example/samgouneili/hackathon/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.samgouneili.hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
+
+
+//make a list for  (suggestions, chat)
+//github
+
+
---
+++ app/src/main/java/com/example/samgouneili/hackathon/SuggestionActivity.java
@@ -0,0 +1,16 @@
+package com.example.samgouneili.hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuggestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suggestion);
+
+
+
+    }
+}
